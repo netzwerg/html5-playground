c1d1b2a3c95d03313c0faf3265fc5db94cb39257
Rahel Lüthy <rahel.luethy@gmail.com>
2012-11-09 15:44:37 +0100

    Total revamp of cube concept (to better fit Rahel's mind)

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
 $(document).ready(function () {
-    var xAngle = 0, zAngle = 0;
+    var tiltedLeft = false, tiltedRight = false;
     $('body').keydown(function (evt) {
+        var xOrigin, zAngle;
         switch (evt.keyCode) {
             case 37: // left
-                zAngle -= 90
-                $('#cube')[0].style["-webkit-transform-origin"] = "bottom left";
-                $('#cube')[0].style["-webkit-transform"] = "rotateZ(" + zAngle + "deg)";
+                if (tiltedRight) {
+                    xOrigin = 400;
+                    zAngle = "-0";
+                    tiltedRight = false
+                } else if (tiltedLeft == false) {
+                    xOrigin = 200;
+                    zAngle = -90;
+                    tiltedLeft = true
+                }
                 break;
-
+            case 39: // right
+                if (tiltedLeft) {
+                    xOrigin = 200;
+                    zAngle = "-0";
+                    tiltedLeft = false
+                } else if (tiltedRight == false) {
+                    xOrigin = 400;
+                    zAngle = 90;
+                    tiltedRight = true
+                }
+                break;
         }
+        var cube = $('#cube')[0];
+        cube.style["-webkit-transform-origin"] = xOrigin + "px 400px";
+        cube.style["-webkit-transform"] = "rotateZ(" + zAngle + "deg)";
     });
 });
