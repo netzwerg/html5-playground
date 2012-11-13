0d56fbc05d29e3eedd5e34be5a7ce577c73e0f64
Rahel Lüthy <rahel.luethy@gmail.com>
2012-11-13 10:52:21 +0100

    Shrunk overall size by 50%

--- js/script.js
+++ js/script.js
@@ -5,29 +5,29 @@ $(document).ready(function () {
         switch (evt.keyCode) {
             case 37: // left
                 if (tiltedRight) {
-                    xOrigin = 400;
+                    xOrigin = 200;
                     zAngle = "-0";
                     tiltedRight = false
                 } else if (tiltedLeft == false) {
-                    xOrigin = 200;
+                    xOrigin = 100;
                     zAngle = -90;
                     tiltedLeft = true
                 }
                 break;
             case 39: // right
                 if (tiltedLeft) {
-                    xOrigin = 200;
+                    xOrigin = 100;
                     zAngle = "-0";
                     tiltedLeft = false
                 } else if (tiltedRight == false) {
-                    xOrigin = 400;
+                    xOrigin = 200;
                     zAngle = 90;
                     tiltedRight = true
                 }
                 break;
         }
         var cube = $('#cube')[0];
-        cube.style["-webkit-transform-origin"] = xOrigin + "px 400px";
+        cube.style["-webkit-transform-origin"] = xOrigin + "px 200px";
         cube.style["-webkit-transform"] = "rotateZ(" + zAngle + "deg)";
     });
 });
